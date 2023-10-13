03d31fcbc12c2379299e0e54683e335a1a7c7a4d
Ian <47256454+IanRFerguson@users.noreply.github.com>
2023-10-13 12:31:44 -0400

    shell commands

---
+++ shell__interactive.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# Start interactive shell in the current terminal
+docker compose exec -it local_parsons_dev sh
---
+++ shell__run.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# Run container script
+docker compose exec local_parsons_dev python dev/airtable_to_bigquery.py
