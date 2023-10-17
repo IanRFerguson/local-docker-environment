349e8d26c81dc1879b4a2bb66314d4dd7a068a07
Ian <47256454+IanRFerguson@users.noreply.github.com>
2023-10-16 21:07:39 -0400

    use clean shell

--- bin/main.sh
+++ bin/main.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
 # Pull down any existing container, compose it back up, and start interactive shell
-docker compose down &&
-docker compose up -d &&
-docker compose exec local_parsons_dev pip install pygit2
+echo "Stopping container..."
+docker compose down > /dev/null 2>&1
+
+echo "Restarting detached container..."
+docker compose up -d  > /dev/null 2>&1
+
+echo "Installing pygit2..."
+docker compose exec local_parsons_dev pip install -r requirements.txt > /dev/null 2>&1
+
+echo "Successfully started container!"
