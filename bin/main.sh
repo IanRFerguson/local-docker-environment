bde2d20b3da4ef8d1980fbeb923940d2ce0716fa
Ian <47256454+IanRFerguson@users.noreply.github.com>
2023-10-16 20:55:12 -0400

    update main

--- bin/main.sh
+++ bin/main.sh
@@ -3,5 +3,4 @@
 # Pull down any existing container, compose it back up, and start interactive shell
 docker compose down &&
 docker compose up -d &&
-docker compose exec local_parsons_dev pip install pygit2 &&
-docker compose exec -it local_parsons_dev sh
+docker compose exec local_parsons_dev pip install pygit2
